59d773833e191e20ae7686aac0ed13e367d74a0c
icortes <icortes@WS-I3-05.WS-ACADBUS.EDU>
2013-10-01 09:23:40 -0700

    initialization of DefendGameView and GameOverView

--- defendthebeast/src/dtb/Defend.java
+++ defendthebeast/src/dtb/Defend.java
@@ -1,11 +1,38 @@
 package dtb;
+
+import java.awt.Color;
+
+import jgame.GRootContainer;
 import jgame.Game;
 import jgame.ImageCache;
+
 public class Defend extends Game {
 
 	public static void main(String[] args) {
 		ImageCache.create(Defend.class, "/dtb/rsc/");
+		Defend d1 = new Defend();
+		d1.startGame();
+	}
+
+	public Defend() {
+
+		GRootContainer root = new GRootContainer(Color.BLACK);
+		setRootContainer(root);
+		DefendGameView dgv = new DefendGameView();
+		GameOverView gov = new GameOverView();
+		root.addView(Views.GAME, dgv);
+		root.addView(Views.GAME_OVER, gov);
 
 	}
 
+	/**
+	 * These are all of the views for this game
+	 * 
+	 * @author eettlin
+	 * 
+	 */
+	public enum Views {
+		// These are all of the views for this game
+		MENU, GAME, GAME_OVER, INSTRUCTIONS, OTHER;
+	}
 }
