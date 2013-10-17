caf7356d9f69a374d905baf240d9468c3bced151
icortes <icortes@WS-I3-07.WS-ACADBUS.EDU>
2013-10-17 09:45:32 -0700

    fixed arrow and set it to Bullet1

--- defendthebeast/src/bullets/Bullet1.java
+++ defendthebeast/src/bullets/Bullet1.java
@@ -2,10 +2,13 @@ package bullets;
 
 import java.awt.Image;
 
+import jgame.ImageCache;
+import dtb.Defend;
+
 public class Bullet1 extends Bullet {
 
 	public Bullet1() {
-		super(image);
+		super(ImageCache.forClass(Defend.class).get("arrow_fix"));
 		// TODO Auto-generated constructor stub
 	}
 
