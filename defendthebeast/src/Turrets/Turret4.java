b9025e8e024be918fb6d587c6c9f9ef1f23ceec0
phernandez <phernandez@WS-I3-21.WS-ACADBUS.EDU>
2013-11-13 09:40:51 -0800

    Turrets wurk

--- defendthebeast/src/Turrets/Turret4.java
+++ defendthebeast/src/Turrets/Turret4.java
@@ -8,7 +8,7 @@ import dtb.Defend;
 public class Turret4 extends Turret{
 
 	public Turret4() {
-		super(ImageCache.forClass(Defend.class).get("Wands/wand4.png"));
+		super(ImageCache.forClass(Defend.class).get("Wands/wand4turret.png"));
 		// TODO Auto-generated constructor stub
 	}
 	public double getFireRange(){
