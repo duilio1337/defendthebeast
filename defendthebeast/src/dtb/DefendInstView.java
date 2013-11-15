703d9b1cda469d9a67a0f9402bcd2eaca09d5de4
rharper <rharper@WS-I3-21.WS-ACADBUS.EDU>
2013-11-15 10:08:13 -0800

    WIP buttons

--- defendthebeast/src/dtb/DefendInstView.java
+++ defendthebeast/src/dtb/DefendInstView.java
@@ -10,19 +10,22 @@ import jgame.Context;
 import jgame.GButton;
 import jgame.GContainer;
 import jgame.GMessage;
+import jgame.GObject;
 import jgame.GSprite;
 import jgame.ImageCache;
+import jgame.controller.Interpolation;
+import jgame.controller.MovementTween;
 import jgame.listener.ButtonListener;
+import jgame.listener.DelayListener;
 
 public class DefendInstView extends GContainer {
 	public DefendInstView() {
 		setSize(1280, 720);
         this.setBackgroundColor(Color.BLACK);
         this.setBackgroundSprite(ImageCache.getSprite("other/MenuScreen.png"));
-		System.out.println("STOP PRESSING MY BUTTONS");
 		
-		GButton mbMM = this.createButton(0, "Play");
-        mbMM.setLocation(100, 200);
+		GButton mbMM = this.createButton(0, "Main Menu");
+        mbMM.setLocation(-100, 100);
         
         ButtonListener blMM = new ButtonListener() {
       	  @Override
@@ -35,24 +38,36 @@ public class DefendInstView extends GContainer {
 }
 
 
-private GButton createButton(final int buttonIndex, String buttonText) {
-    
-    final GButton btn = new GButton();
-    btn.setStateSprite(ButtonState.NONE, createButtonSprite("Tiles/menubarnew.png"));
-    btn.setStateSprite(ButtonState.HOVERED, createButtonSprite("Tiles/selectnew.png"));
-    btn.setStateSprite(ButtonState.PRESSED, createButtonSprite("Tiles/pressedbarnew.png"));
-    btn.setSize(250, 55);
-    GMessage gm = new GMessage(buttonText);
-    
-    gm.setSize(btn.getWidth(), btn.getHeight());
-    gm.setAlignmentX(0.5);
-    gm.setAlignmentY(0.5);
-    gm.setFontSize(28);
-    gm.setColor(Color.BLACK);
-    btn.addAtCenter(gm);
-    
-    return btn;
-}
+ private GButton createButton(final int buttonIndex, String buttonText) {
+         
+         MovementTween mt = new MovementTween(24, Interpolation.EASE, 400, 0);
+         MovementTween mtb = new MovementTween(6, Interpolation.EASE, -40, 0);
+         mt.chain(mtb);
+         final GButton btn = new GButton();
+         btn.addController(mt);
+         btn.setStateSprite(ButtonState.NONE, createButtonSprite("Tiles/menubarnew.png"));
+         btn.setStateSprite(ButtonState.HOVERED, createButtonSprite("Tiles/selectnew.png"));
+         btn.setStateSprite(ButtonState.PRESSED, createButtonSprite("Tiles/pressedbarnew.png"));
+         btn.setSize(250, 55);
+         GMessage gm = new GMessage(buttonText);
+         
+         gm.setSize(btn.getWidth(), btn.getHeight());
+         gm.setAlignmentX(0.5);
+         gm.setAlignmentY(0.5);
+         gm.setFontSize(28);
+         gm.setColor(Color.BLACK);
+         btn.addAtCenter(gm);
+         
+         DelayListener dl = new DelayListener(buttonIndex * 10) {
+                 
+                 @Override
+                 public void invoke(GObject target, Context context) {
+                         addAt(btn, -100, buttonIndex * 100+200);
+                 }
+         };
+         addListener(dl);
+         return btn;
+ }
 
 public static GSprite createButtonSprite(String fn) {
     BufferedImage img = ImageCache.forClass(Defend.class).get(fn);
@@ -61,5 +76,5 @@ public static GSprite createButtonSprite(String fn) {
     Rectangle nineSliceCenter = new Rectangle(15, 15, 6, 6);
     gs.setNineSliceCenter(nineSliceCenter);
     return gs;
-}
+	}
 }
