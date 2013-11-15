f2f1bffd4447ffe6fbeccfbb45c14313cc18d5ae
icortes <icortes@WS-I3-22.WS-ACADBUS.EDU>
2013-11-15 10:07:07 -0800

    dgfsdgfdgfdgf

--- defendthebeast/src/Enemies/Boss.java
+++ defendthebeast/src/Enemies/Boss.java
@@ -1,8 +1,13 @@
 package Enemies;
 
 import java.awt.Image;
+import java.util.List;
 
+import jgame.Context;
+import jgame.GObject;
 import jgame.listener.BoundaryRemovalListener;
+import jgame.listener.FrameListener;
+import Turrets.Turret;
 
 public abstract class Boss extends Enemy{
 	public Boss(Image image, double health) {
