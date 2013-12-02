4687982f54885ddb6e54fd0d528e702efa7b410e
phernandez <phernandez@WS-I3-11.WS-ACADBUS.EDU>
2013-12-02 10:06:23 -0800

    Moar lightnings

--- defendthebeast/src/bullets/Bullet3.java
+++ defendthebeast/src/bullets/Bullet3.java
@@ -1,26 +1,39 @@
 package bullets;
 
+import jgame.Context;
+import jgame.GObject;
 import jgame.ImageCache;
+import jgame.listener.BoundaryRemovalListener;
+import jgame.listener.ButtonListener;
+import jgame.listener.DelayListener;
+import jgame.listener.HitTestListener;
 import dtb.Defend;
+
 import java.awt.Image;
+import java.awt.Point;
+import java.util.List;
+
+import Enemies.Enemy;
+import Turrets.Turret;
 
 public class Bullet3 extends Bullet {
-
+	
+	private double bd;
+	private Point initialPosition = new Point();
 	public Bullet3() {
-		super(randImg(), Integer.MAX_VALUE, "Enemy");
-		// TODO Auto-generated constructor stub
+		super(randImg(), 200, "Enemy");
 	}
 	private static Image randImg(){
-		String Img = "Bullets/lightning1.png";
-		double a = (Math.random()*3);
+		String Img;
+		double a = (Math.random()*4);
 		if(a<1){
 			Img = "Bullets/lightning1.png";
 		}else if(a<2){
 			Img = "Bullets/lightning2.png";
 		}else if(a<3){
-			Img = "Bullets/lightning1.png";
+			Img = "Bullets/lightning3.png";
 		}else{
-			Img = "Bullets/lightning2.png";
+			Img = "Bullets/lightning4.png";
 		}
 		return ImageCache.forClass(Defend.class).get(Img);
 	}
