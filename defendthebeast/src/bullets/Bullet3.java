3dba0a5d759c47dd22393567b3e9fa5a49cc9da8
phernandez <phernandez@WS-I3-11.WS-ACADBUS.EDU>
2013-12-02 09:13:36 -0800

    Lightning Bullets!

--- defendthebeast/src/bullets/Bullet3.java
+++ defendthebeast/src/bullets/Bullet3.java
@@ -2,12 +2,27 @@ package bullets;
 
 import jgame.ImageCache;
 import dtb.Defend;
+import java.awt.Image;
 
 public class Bullet3 extends Bullet {
 
 	public Bullet3() {
-		super(ImageCache.forClass(Defend.class).get("Bullets/arrow_fix.png"), 10, "Enemy");
+		super(randImg(), Integer.MAX_VALUE, "Enemy");
 		// TODO Auto-generated constructor stub
+	}
+	private static Image randImg(){
+		String Img = "Bullets/lightning1.png";
+		double a = (Math.random()*3);
+		if(a<1){
+			Img = "Bullets/lightning1.png";
+		}else if(a<2){
+			Img = "Bullets/lightning2.png";
+		}else if(a<3){
+			Img = "Bullets/lightning1.png";
+		}else{
+			Img = "Bullets/lightning2.png";
+		}
+		return ImageCache.forClass(Defend.class).get(Img);
 	}
 
 }
