405094c0e2bc5739543171e98236d42b7f4ee1b6
rharper <rharper@WS-I3-13.WS-ACADBUS.EDU>
2013-12-10 12:47:50 -0800

    removed more inports

--- defendthebeast/src/Enemies/Boss.java
+++ defendthebeast/src/Enemies/Boss.java
@@ -1,16 +1,8 @@
 package Enemies;
 
 import java.awt.Image;
-import java.util.List;
-import jgame.Context;
-import jgame.GObject;
-import jgame.controller.ConstantMovementController;
-import jgame.listener.DelayListener;
-import jgame.listener.FrameListener;
 import Enemies.Enemy;
-import bullets.Bullet;
 import jgame.listener.BoundaryRemovalListener;
-import Turrets.Turret;
 
 
 public abstract class Boss extends Enemy{
