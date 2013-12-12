b6f9098716d60a08c39375315d2fd803e610c694
rharper <rharper@WS-I3-12.WS-ACADBUS.EDU>
2013-12-12 10:45:36 -0800

    tiles and stoof working

--- defendthebeast/src/areas/Tile.java
+++ defendthebeast/src/areas/Tile.java
@@ -1,23 +1,34 @@
 package areas;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import jgame.ButtonState;
 import jgame.Context;
 import jgame.GButton;
 import jgame.GSprite;
 import jgame.ImageCache;
+import jgame.controller.MouseLocationController;
 import jgame.listener.ButtonListener;
 import Turrets.Turret;
+import dtb.Defend;
 import dtb.DefendGameView;
 
 public class Tile extends GButton {
 	TurretRecipe tr;
+	
+	GSprite infocard1 = new GSprite(ImageCache.forClass(Defend.class).get("Other/info-card1.png"));
+	GSprite infocard2 = new GSprite(ImageCache.forClass(Defend.class).get("Other/info-card2.png"));
+	GSprite infocard3 = new GSprite(ImageCache.forClass(Defend.class).get("Other/info-card3.png"));
+	GSprite infocard4 = new GSprite(ImageCache.forClass(Defend.class).get("Other/info-card4.png"));
+	GSprite infocard5 = new GSprite(ImageCache.forClass(Defend.class).get("Other/info-card5.png"));
 
 	public Tile(TurretRecipe recipe, Image image, final int turNum) {
 		this.tr = recipe;
 		
 		setSize(66, 66);
+		
+		
 
 		GSprite sNone = ImageCache.getSprite("Tiles/menubarnew.png");
 		GSprite sHover = ImageCache.getSprite("Tiles/selectnew.png");
@@ -41,10 +52,57 @@ public class Tile extends GButton {
 					Turret turret = tr.createTurret();
 					getFirstAncestorOf(DefendGameView.class).initializeTurret(turret);
 				}else{
-					getFirstAncestorOf(DefendGameView.class).newHUDMessage("Insufficient funds", 60);
+					getFirstAncestorOf(DefendGameView.class).newHUDMessage("Insufficient funds", 60, 3);
 				}
 			}
 
+		});
+		
+		addListener(new ButtonListener() {
+			@Override
+			public void mouseOver(Context context) {
+				switch(turNum) {
+				case 1:
+					getFirstAncestorOf(DefendGameView.class).addAt(infocard1, getX()+30, 580);
+					break;
+				case 2:
+					getFirstAncestorOf(DefendGameView.class).addAt(infocard2, getX()+30, 580);
+					break;
+				case 3:
+					getFirstAncestorOf(DefendGameView.class).addAt(infocard3, getX()+30, 580);
+					break;
+				case 4:
+					getFirstAncestorOf(DefendGameView.class).addAt(infocard4, getX()+30, 580);
+					break;
+				case 5:
+					getFirstAncestorOf(DefendGameView.class).addAt(infocard5, getX()+30, 580);
+					break;
+				}
+				
+			}
+		});
+		
+		addListener(new ButtonListener() {
+			@Override
+			public void mouseOut(Context context) {
+				switch(turNum) {
+				case 1:
+					remove(infocard1);
+					break;
+				case 2:
+					remove(infocard2);
+					break;
+				case 3:
+					remove(infocard3);
+					break;
+				case 4:
+					remove(infocard4);
+					break;
+				case 5:
+					remove(infocard5);
+					break;
+				}
+			}
 		});
 
 	}
